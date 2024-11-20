652410028fc6841dc88a3ba373db11c3f8bdc832
Liam Mullen <41468599+lmullen534@users.noreply.github.com>
2024-11-20 15:25:20 +0000

    Created an example Architecture Unit Test Class using Arch Unit Test  (#9)
    * Created an example Architecture Unit Test Class
    
    * Test Tidy Up (#2)
    
    * Cleaing up project references (#3)

---
+++ tests/ECommerce.ArchitectureTests/BaseArchitecture.cs
@@ -0,0 +1,23 @@
+﻿using ArchUnitNET.Domain;
+using ArchUnitNET.Loader;
+
+namespace ECommerce.ArchitectureTests;
+
+public abstract class BaseArchitecture
+{
+    private const string BaseNamespace = "ECommerce";
+
+    protected const string ModulesNamespace = $"{BaseNamespace}.Modules";
+    protected const string CustomersModuleNamespace = $"{ModulesNamespace}.Customers";
+    protected const string OrdersModuleNamespace = $"{ModulesNamespace}.Orders";
+    protected const string ProductsModuleNamespace = $"{ModulesNamespace}.Products";
+
+    protected readonly Architecture _architecture = new ArchLoader().LoadAssemblies(
+        typeof(Contracts.Interfaces.ICustomerCatalogService).Assembly,
+        typeof(Contracts.Interfaces.IProductCatalogService).Assembly,
+        typeof(Common.Domain.Entity).Assembly,
+        typeof(Modules.Orders.Services.OrderService).Assembly,
+        typeof(Modules.Products.Services.ProductService).Assembly,
+        typeof(Modules.Customers.Services.CustomerService).Assembly,
+        typeof(Modules.Customers.Services.CustomerCatalogService).Assembly).Build();
+}
---
+++ tests/ECommerce.ArchitectureTests/ModuleTests/CustomerArchitectureTests.cs
@@ -0,0 +1,62 @@
+﻿using ArchUnitNET.Domain;
+using ArchUnitNET.Fluent;
+using ArchUnitNET.xUnit;
+using ECommerce.Common.Domain;
+
+namespace ECommerce.ArchitectureTests.ModuleTests;
+
+public class CustomerArchitectureTests : BaseArchitecture
+{
+    private const string CustomersDomainNamespace = $"{CustomersModuleNamespace}.Domain";
+
+    [Fact]
+    public void Modules_Should_Not_Depend_On_Each_Other()
+    {
+        IObjectProvider<IType> ordersClasses =
+            ArchRuleDefinition.Types().That().ResideInNamespace(OrdersModuleNamespace, true);
+        IObjectProvider<IType> productsClasses =
+            ArchRuleDefinition.Types().That().ResideInNamespace(ProductsModuleNamespace, true);
+        IObjectProvider<IType> customersClasses =
+            ArchRuleDefinition.Types().That().ResideInNamespace(CustomersModuleNamespace, true);
+
+        var ordersRule = ArchRuleDefinition.Types().That().Are(ordersClasses)
+            .Should().NotDependOnAny(productsClasses).AndShould().NotDependOnAny(customersClasses);
+
+        var productsRule = ArchRuleDefinition.Types().That().Are(productsClasses)
+            .Should().NotDependOnAny(ordersClasses).AndShould().NotDependOnAny(customersClasses);
+
+        var customersRule = ArchRuleDefinition.Types().That().Are(customersClasses)
+            .Should().NotDependOnAny(ordersClasses).AndShould().NotDependOnAny(productsClasses);
+
+        ordersRule.Check(_architecture);
+        productsRule.Check(_architecture);
+        customersRule.Check(_architecture);
+    }
+
+    [Fact]
+    public void Module_Namespaces_Should_Be_Prefixed_With_Modules()
+    {
+        IObjectProvider<IType> moduleClasses = ArchRuleDefinition.Types()
+            .That().ResideInNamespace(ModulesNamespace, true);
+
+        // Enforce the rule
+        var rule = ArchRuleDefinition.Types().That().Are(moduleClasses)
+            .Should().ResideInNamespace(ModulesNamespace, true);
+
+        rule.Check(_architecture);
+    }
+
+    [Fact]
+    public void Domain_Classes_Should_Inherit_From_Entity()
+    {
+        // Arrange & Act
+        var rule = ArchRuleDefinition.Classes()
+            .That()
+            .ResideInNamespace(CustomersDomainNamespace)
+            .Should()
+            .BeAssignableTo(typeof(Entity));
+
+        // Assert
+        rule.Check(_architecture);
+    }
+}
