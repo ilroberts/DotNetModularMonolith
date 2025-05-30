9c462eaee7925b8e4d7ade830ae5ce4c6fe24d27
Iwan Roberts <iwanr+gwc@kainos.com>
2025-05-30 16:40:28 +0100

    products added

--- ECommerce.Modules.Products/ProductModule.cs
+++ ECommerce.Modules.Products/ProductModule.cs
@@ -4,22 +4,41 @@ using Microsoft.Extensions.Configuration;
 using ECommerce.Contracts.Interfaces;
 using ECommerce.Modules.Products.Persistence;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace ECommerce.Modules.Products;
 
 public static class ProductModule
 {
     public static IServiceCollection AddProductModule(this IServiceCollection services, IConfiguration configuration)
-  {
-
-    services.AddDbContext<ProductDbContext>(options =>
     {
-      options.UseInMemoryDatabase("ECommerce.Product");
-    });
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        var serviceProvider = services.BuildServiceProvider();
+        var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+        var logger = loggerFactory?.CreateLogger("ProductModule");
 
-    services.AddScoped<IProductService, ProductService>();
-    services.AddScoped<IProductCatalogService, ProductCatalogService>();
+        services.AddDbContext<ProductDbContext>(options =>
+        {
+            if (!string.IsNullOrEmpty(connectionString))
+            {
+                logger?.LogInformation("ProductModule: Using PostgreSQL connection string: {ConnectionString}",
+                    connectionString.Substring(0, Math.Min(50, connectionString.Length)) + "...");
+
+                options.UseNpgsql(connectionString, npgsqlOptions =>
+                {
+                    npgsqlOptions.MigrationsAssembly("ECommerce.Modules.Products");
+                });
+            }
+            else
+            {
+                logger?.LogInformation("ProductModule: No connection string found, using in-memory database");
+                options.UseInMemoryDatabase("ECommerce.Product");
+            }
+        });
+
+        services.AddScoped<IProductService, ProductService>();
+        services.AddScoped<IProductCatalogService, ProductCatalogService>();
 
         return services;
-  }
+    }
 }
