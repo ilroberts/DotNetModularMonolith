dbb3303576641a469aeca10fc4d816bffcf71181
Iwan Roberts <iwanr+gwc@kainos.com>
2025-06-04 14:21:06 +0100

    added logout

---
+++ ECommerce.AdminUI/Pages/Logout.cshtml.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ECommerce.AdminUI.Pages;
+
+public class LogoutModel : PageModel
+{
+    private readonly ILogger<LogoutModel> _logger;
+
+    public LogoutModel(ILogger<LogoutModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult OnGet()
+    {
+        // Log the logout event
+        var username = HttpContext.Session.GetString("Username");
+        _logger.LogInformation("User {Username} logged out", username);
+
+        // Clear all session data
+        HttpContext.Session.Clear();
+
+        // Redirect to login page
+        return RedirectToPage("/Login");
+    }
+}
